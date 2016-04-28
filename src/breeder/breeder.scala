0c1f3d59ddd249de009db07c78b9b7ab8cc4c2d4
magoeke <magoeke@htwg-konstanz.de>
2016-04-28 13:49:08 +0200

    First usage of Option Monad. It's used in a very simple way.

--- src/breeder/breeder.scala
+++ src/breeder/breeder.scala
@@ -1,23 +1,21 @@
 package breeder
 
-class PetOwner(pet: Pet) { def getPet: Pet = pet }
-class Pet { def getBreeder: Breeder = new Breeder }
+class PetOwner(pet: Pet) { def getPet: Option[Pet] = Option(pet) }
+class Pet { def getBreeder: Option[Breeder] = Option(new Breeder) }
 class Breeder { def getName: String = "Georg" }
 
 object breeder {
+  
+  // This methods are created to handle Option step by step
+  def findOutName(breeder: Breeder): String = breeder.getName
+  def findOutBreeder(pet: Pet): Option[String] = pet.getBreeder.map(findOutName(_))
+  def findOutPet(petOwner: PetOwner): Option[String] = petOwner.getPet.flatMap(findOutBreeder(_))
+  
   def main(args: Array[String]) {
     var hans = new PetOwner(new Pet())
-    hans.getPet.getBreeder.getName
-
     var moritz = new PetOwner(null)
-    // nested if to avoid NullPointerException
-    // other possible solution is try catch
-    if(moritz.getPet != null) {
-      if(moritz.getPet.getBreeder != null) {
-        moritz.getPet.getBreeder.getName
-      } else
-        null
-    } else
-      null    
+    
+    findOutPet(hans)
+    findOutPet(moritz)
   }
 }
