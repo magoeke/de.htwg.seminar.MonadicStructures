b4738f1f71f1793612a9df6d433ff6e31549c4b0
magoeke <magoeke@htwg-konstanz.de>
2016-04-28 20:01:23 +0200

    Created one method that handles the option monad and deleted the others.

--- src/breeder/breeder.scala
+++ src/breeder/breeder.scala
@@ -5,24 +5,16 @@ class Pet { def getBreeder: Option[Breeder] = Option(new Breeder) }
 class Breeder { def getName: String = "Georg" }
 
 object breeder {
-  
-  // This methods are created to handle Option step by step
-  def findOutName(breeder: Breeder): String = breeder.getName
-  def findOutBreeder(pet: Pet): Option[String] = pet.getBreeder.map(findOutName(_))
-  def findOutPet(petOwner: PetOwner): Option[String] = petOwner.getPet.flatMap(findOutBreeder(_))
-  
   def main(args: Array[String]) {
     var hans = new PetOwner(new Pet())
     var moritz = new PetOwner(null)
-    
-    findOutPet(hans)
-    findOutPet(moritz)
-    
-    //Add some output for understanding
-    println(findOutPet(moritz))     // None
-    findOutPet(moritz).map(println) // ""
-    println(findOutPet(hans))       // Some(Georg)
-    findOutPet(hans).map(println)   // Georg
-    
+ 
+    getBreeder(hans);
+    getBreeder(moritz);
+  }
+  
+  def getBreeder(petOwner: PetOwner): Option[String] = {
+    petOwner.getPet.flatMap(pet => 
+      pet.getBreeder.map( breeder => breeder.getName))
   }
 }
